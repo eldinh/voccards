3d1e0c4b9e71f932f8fc8885b7e7f6e560199ade
notAJavaEnjoyer <mr.eldinh01@gmail.com>
2022-03-24 08:41:52 +0300

    comm

--- src/main/java/ru/sfedu/voccards/service/MainService.java
+++ src/main/java/ru/sfedu/voccards/service/MainService.java
@@ -3,7 +3,9 @@ package ru.sfedu.voccards.service;
 import org.springframework.http.ResponseEntity;
 
 public interface MainService {
+
     ResponseEntity<?> addRoleToUser(String username, String role);
+
     ResponseEntity<?> getCardSetById(Long id);
 
 }
